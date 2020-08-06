64e724774bf947b13a959ebc7ec9c5a08e767f51
Sergey Khrebtov <sergey.khrebtov@sibers.com>
2020-08-06 16:23:33 +0700

    add animations

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import Uimini from "uimini/dist/css/uimini.css";
 import router from "./router";
 import store from "./store";
 
+import "vue2-animate/dist/vue2-animate.min.css";
+
 Vue.use(Vuelidate, Uimini);
 
 Vue.config.productionTip = false;
